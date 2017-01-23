e28845136292b59501e2a4b1f3885b833466f7c9
Chris Picato <christopherpicato@gmail.com>
2017-01-23 00:36:05 -0800

    Change file structure

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ if (process.env.NODE_ENV !== 'production') {
 }
 
 app.get('*', function(req, res) {
-  res.sendFile(path.join(__dirname, 'index.html'));
+  res.sendFile(path.join(__dirname, 'public/index.html'));
 });
 
 app.listen(port, function(err) {
