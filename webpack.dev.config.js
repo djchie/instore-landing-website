1c3a47e7fbbcf6470b3106ae7babdb0b0cfb2cf8
Chris Picato <christopherpicato@gmail.com>
2017-01-22 23:41:31 -0800

    Change file system for production

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -1,19 +1,55 @@
-var path = require('path');
-var webpack = require('webpack');
+// var path = require('path');
+// var webpack = require('webpack');
+//
+// module.exports = {
+//   devtool: 'cheap-module-eval-source-map',
+//   entry: [
+//     './src/index'
+//   ],
+//   output: {
+//     path: path.join(__dirname, 'dist'),
+//     filename: 'bundle.js',
+//     publicPath: '/static/'
+//   },
+//   plugins: [
+//     new webpack.HotModuleReplacementPlugin(),
+//   ],
+//   module: {
+//     loaders: [{
+//       test: /\.js$/,
+//       loaders: ['react-hot', 'babel'],
+//       include: path.join(__dirname, 'src')
+//     },
+//     {
+//       test: /\.css$/,
+//       loader: 'style!css!',
+//     }
+//     ]
+//   }
+// };
+
+const path = require('path');
+const webpack = require('webpack');
 
 module.exports = {
-  devtool: 'cheap-module-eval-source-map',
+  devtool: 'eval',
+
   entry: [
+    'webpack-hot-middleware/client',
     './src/index'
   ],
+
   output: {
-    path: path.join(__dirname, 'dist'),
+    path: path.join(__dirname, 'public'),
     filename: 'bundle.js',
-    publicPath: '/static/'
+    publicPath: '/public/'
   },
+
   plugins: [
     new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoErrorsPlugin()
   ],
+
   module: {
     loaders: [{
       test: /\.js$/,
--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -1,28 +1,80 @@
-var path = require('path');
-var webpack = require('webpack');
+// var path = require('path');
+// var webpack = require('webpack');
+//
+// module.exports = {
+//   devtool: 'cheap-module-eval-source-map',
+//   entry: [
+//     './src/index'
+//   ],
+//   output: {
+//     path: path.join(__dirname, 'public'),
+//     filename: 'bundle.js',
+//     publicPath: '/public/'
+//   },
+//   plugins: [
+//     new webpack.optimize.DedupePlugin(),
+//     new webpack.optimize.UglifyJsPlugin({
+//       minimize: true,
+//       compress: {
+//         warnings: false
+//       }
+//     }),
+//     new webpack.DefinePlugin({
+//       'process.env': {
+//         'NODE_ENV': JSON.stringify('production')
+//       }
+//     }),
+//     new webpack.DefinePlugin({
+//       'process.env': {
+//         'HOST_ENV': '"web"'
+//       }
+//     })
+//   ],
+//   module: {
+//     loaders: [{
+//       test: /\.js$/,
+//       loaders: ['react-hot', 'babel'],
+//       include: path.join(__dirname, 'src')
+//     },
+//     {
+//       test: /\.css$/,
+//       loader: 'style!css!',
+//     }
+//     ]
+//   }
+// };
+
+const path = require('path');
+const webpack = require('webpack');
 
 module.exports = {
-  devtool: 'cheap-module-eval-source-map',
+  devtool: 'source-map',
+
   entry: [
     './src/index'
   ],
+
   output: {
-    path: path.join(__dirname, 'dist'),
+    path: path.join(__dirname, 'public'),
     filename: 'bundle.js',
-    publicPath: '/static/'
+    publicPath: '/public/'
   },
+
   plugins: [
-    new webpack.DefinePlugin({
-      'process.env': {
-        'NODE_ENV': '"production"'
+    new webpack.optimize.DedupePlugin(),
+    new webpack.optimize.UglifyJsPlugin({
+      minimize: true,
+      compress: {
+        warnings: false
       }
     }),
     new webpack.DefinePlugin({
       'process.env': {
-        'HOST_ENV': '"web"'
+        'NODE_ENV': JSON.stringify('production')
       }
     })
   ],
+
   module: {
     loaders: [{
       test: /\.js$/,
