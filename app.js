57e7600586432eb6714388f7ca639168c639108e
Chris Picato <christopherpicato@gmail.com>
2017-01-22 21:01:26 -0800

    Setup for heroku push

--- app.js
+++ app.js
@@ -2,24 +2,26 @@ var path = require('path');
 var webpack = require('webpack');
 var express = require('express');
 var config = require('./webpack.config');
+var port = process.env.PORT || 3000;
 
 var app = express();
 var compiler = webpack(config);
 
-app.use(require('webpack-dev-middleware')(compiler, {
-  publicPath: config.output.publicPath
-}));
+if (process.env.NODE_ENV !== 'production') {
+  app.use(require('webpack-dev-middleware')(compiler, {
+    publicPath: config.output.publicPath
+  }));
 
-app.use(require('webpack-hot-middleware')(compiler));
+  app.use(require('webpack-hot-middleware')(compiler));
+}
 
 app.get('*', function(req, res) {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-app.listen(3000, function(err) {
+app.listen(port, function(err) {
   if (err) {
     return console.error(err);
   }
-
-  console.log('Listening at http://localhost:3000/');
+  console.log('Server listening on: ' + port);
 });
