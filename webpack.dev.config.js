76ce3e9ac072857973f83eb7bf3d2721c6c47e29
Chris Picato <christopherpicato@gmail.com>
2017-01-26 17:28:59 -0800

    Remove react-hot from prod webpack

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -1,33 +1,3 @@
-// var path = require('path');
-// var webpack = require('webpack');
-//
-// module.exports = {
-//   devtool: 'cheap-module-eval-source-map',
-//   entry: [
-//     './src/index'
-//   ],
-//   output: {
-//     path: path.join(__dirname, 'dist'),
-//     filename: 'bundle.js',
-//     publicPath: '/static/'
-//   },
-//   plugins: [
-//     new webpack.HotModuleReplacementPlugin(),
-//   ],
-//   module: {
-//     loaders: [{
-//       test: /\.js$/,
-//       loaders: ['react-hot', 'babel'],
-//       include: path.join(__dirname, 'src')
-//     },
-//     {
-//       test: /\.css$/,
-//       loader: 'style!css!',
-//     }
-//     ]
-//   }
-// };
-
 const path = require('path');
 const webpack = require('webpack');
 
--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -1,49 +1,3 @@
-// var path = require('path');
-// var webpack = require('webpack');
-//
-// module.exports = {
-//   devtool: 'cheap-module-eval-source-map',
-//   entry: [
-//     './src/index'
-//   ],
-//   output: {
-//     path: path.join(__dirname, 'public'),
-//     filename: 'bundle.js',
-//     publicPath: '/public/'
-//   },
-//   plugins: [
-//     new webpack.optimize.DedupePlugin(),
-//     new webpack.optimize.UglifyJsPlugin({
-//       minimize: true,
-//       compress: {
-//         warnings: false
-//       }
-//     }),
-//     new webpack.DefinePlugin({
-//       'process.env': {
-//         'NODE_ENV': JSON.stringify('production')
-//       }
-//     }),
-//     new webpack.DefinePlugin({
-//       'process.env': {
-//         'HOST_ENV': '"web"'
-//       }
-//     })
-//   ],
-//   module: {
-//     loaders: [{
-//       test: /\.js$/,
-//       loaders: ['react-hot', 'babel'],
-//       include: path.join(__dirname, 'src')
-//     },
-//     {
-//       test: /\.css$/,
-//       loader: 'style!css!',
-//     }
-//     ]
-//   }
-// };
-
 const path = require('path');
 const webpack = require('webpack');
 
@@ -78,7 +32,7 @@ module.exports = {
   module: {
     loaders: [{
       test: /\.js$/,
-      loaders: ['react-hot', 'babel'],
+      loaders: ['react', 'babel'],
       include: path.join(__dirname, 'src')
     },
     {
