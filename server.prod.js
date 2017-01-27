05b77e7a3e9c4cc835b8d47b0d9ad10ff5b56380
Chris Picato <christopherpicato@gmail.com>
2017-01-26 17:20:35 -0800

    Edit server prod to match dev

--- server.prod.js
+++ server.prod.js
@@ -1,5 +1,8 @@
 var express = require('express');
+var rp = require('request-promise');
+
 var app = express();
+var config = require('./src/config.js');
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -8,6 +11,32 @@ app.use(express.static(__dirname + '/public'));
 // views is directory for all template files
 app.get('/', function(request, response) {
   response.sendFile(__dirname + '/index.html');
+});
+
+app.post('/signup', (req, res) => {
+  var obj = {
+    'email_address': req.body.email,
+    status: 'subscribed',
+  };
+
+  rp({
+    method: 'POST',
+    url: `https://us15.api.mailchimp.com/3.0/lists/${config.MAILCHIMP_LIST}/members`,
+    auth: {
+      user: 'any',
+      password: config.MAILCHIMP_API,
+    },
+    header: {
+      'Content-Type': 'application/json',
+      'Authorization': `apikey ${config.MAILCHIMP_API}`,
+    },
+    json: obj,
+  })
+  .then((success) => {
+    res.status(200).send(success);
+  }).catch((err) => {
+    res.status(500).send(err);
+  });
 });
 
 app.listen(app.get('port'), function() {
